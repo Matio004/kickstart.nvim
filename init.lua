f4436ad00c014371804f150d57a02a500b228cc9
Matio004 <mateuszkupis@interia.eu>
2026-04-08 21:38:08 +0200

    Fix markdown linting

--- init.lua
+++ init.lua
@@ -621,6 +621,9 @@ require('lazy').setup({
 
         stylua = {}, -- Used to format Lua code
 
+        -- Markdown
+        markdownlint = {},
+
         -- Special Lua Config, as recommended by neovim help docs
         lua_ls = {
           on_init = function(client)
