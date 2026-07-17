589826372f12688e39932725415339f07b968d78
Matio004 <mateuszkupis@interia.eu>
2026-07-17 21:38:47 +0200

    add bufferline

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,20 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require('bufferline').setup {
+      options = {
+        -- Podstawowe, bardzo wygodne ustawienia:
+        mode = 'buffers', -- pokazuj bufory (nie prawdziwe karty)
+        diagnostics = 'nvim_lsp', -- pokazuj błędy z lsp na zakładkach
+        separator_style = 'slant', -- "slant" (ukośne), "thick" (grube) lub "thin" (cienkie)
+        always_show_bufferline = true, -- zawsze pokazuj pasek, nawet przy jednym pliku
+        show_buffer_close_icons = true,
+        show_close_icon = false,
+      },
+    }
+  end,
+}
