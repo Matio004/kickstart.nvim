53f6151cf215b870f3dcb6cf799f8eb292a5df2d
Matio004 <mateuszkupis@interia.eu>
2026-07-20 23:04:28 +0200

    fuzzy prefer_rust, friendly-snippets

--- init.lua
+++ init.lua
@@ -736,12 +736,15 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              local luasnip = require 'luasnip'
+              luasnip.filetype_extend('python', { 'django' })
+              luasnip.filetype_extend('html', { 'htmldjango' })
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
         opts = {},
       },
@@ -802,7 +805,7 @@ require('lazy').setup({
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = { implementation = 'prefer_rust' },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
