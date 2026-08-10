a8d3b4add5789da8144bf3567a6e3d9fc29a9177
Matio004 <mateuszkupis@interia.eu>
2026-08-10 19:05:10 +0200

    fix loading issue

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -24,7 +24,4 @@ return {
       lsp_doc_border = false, -- add a border to hover docs and signature help
     },
   },
-  require('render-markdown').setup {
-    latex = { enabled = false },
-  },
 }
