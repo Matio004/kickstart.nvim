0ab8f15eb63dbdb194e8f496545d33d4d1c43c5c
Matio004 <mateuszkupis@interia.eu>
2026-08-25 22:17:32 +0200

    initial dashboard config

---
+++ lua/custom/plugins/dashboard.lua
@@ -0,0 +1,12 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      -- config
+    }
+  end,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+}
