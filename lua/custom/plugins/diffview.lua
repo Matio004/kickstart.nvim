67ab37ab75f6fdfc8170d5d80d445b70efb3d078
Matio004 <mateuszkupis@interia.eu>
2026-08-28 20:12:12 +0200

    add keymaps, change diff4_nixed, diffopt

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -6,16 +6,24 @@ return {
     'nvim-lua/plenary.nvim',
   },
   config = function()
-    vim.opt.diffopt:append 'linematch:60'
-
     require('diffview').setup {
       enhanced_diff_hl = true,
       use_icons = true,
 
       view = {
         merge_tool = {
-          layout = 'diff3_mixed',
+          layout = 'diff4_mixed',
           disable_diagnostics = true,
+        },
+      },
+      keymaps = {
+        view = {
+          { 'n', '<leader>co', require('diffview.actions').conflict_choose 'ours', { desc = 'Choose OURS' } },
+          { 'n', '<leader>ct', require('diffview.actions').conflict_choose 'theirs', { desc = 'Choose THEIRS' } },
+          { 'n', '<leader>cb', require('diffview.actions').conflict_choose 'base', { desc = 'Choose BASE' } },
+          { 'n', '<leader>ca', require('diffview.actions').conflict_choose 'all', { desc = 'Choose ALL' } },
+          { 'n', ']x', require('diffview.actions').next_conflict, { desc = 'Next conflict' } },
+          { 'n', '[x', require('diffview.actions').prev_conflict, { desc = 'Prev conflict' } },
         },
       },
     }
--- lua/options.lua
+++ lua/options.lua
@@ -1,1 +1,9 @@
 vim.opt.sessionoptions = { 'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals', 'skiprtp', 'folds' }
+vim.opt.diffopt = {
+  'internal',
+  'filler',
+  'closeoff',
+  'indent-heuristic',
+  'inline:char',
+  'algorithm:histogram',
+}
